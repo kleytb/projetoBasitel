cb07428f1fec2e0c5b736c51fff859e28b4553a6
Kley Taboada <kleytb@gmail.com>
2022-02-02 19:52:27 -0300

    Main Base

---
+++ www/site-basitel/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import './App.css'
+
+const App = () => {
+    return(
+        <div className="box-container">
+            <h1>ESTAMOS EM CONSTRUÇÃO</h1>
+        </div>
+    );
+}
+
+export default App;
